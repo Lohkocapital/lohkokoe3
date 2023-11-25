1a8f2e96437457ef1744f6795956bbb5d6b7fd82
Sami Salminen <48570580+Lohkocapital@users.noreply.github.com>
2023-11-25 13:56:14 +0200

    Update main.py
    Tahan kuvaus siita mita muutoksia tehtiin. Lisatty kommentti inline-editista browserissa ja mahdollisesta Aaron editorista.
    
    Commit suoraan paahaaraan.

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ import datetime
 print("Repo lohkokoe3: Ensimmainen lisatty file: main.py")
 aika = datetime.datetime.now()
 print(f"Nyt on {aika}")
+
+# Kokeillaan in-place editia suoraan browserin editorissa
+# Toimisiko ehka Aaron devieditorina koulussa?
