17f54c2f51dd36ed9f21a3fc3a08d70b93664a47
Sami Salminen <48570580+Lohkocapital@users.noreply.github.com>
2023-11-25 12:21:27 +0000

    Tähän lisäkommentit ääkkösillä

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import datetime
+import pandas
 
 print("Repo lohkokoe3: Ensimmainen lisatty file: main.py")
 aika = datetime.datetime.now()
@@ -6,3 +7,7 @@ print(f"Nyt on {aika}")
 
 # Kokeillaan in-place editia suoraan browserin editorissa
 # Toimisiko ehka Aaron devieditorina koulussa?
+
+# Lisataan pandas ja kokeillaan samalla codespacesia
+print(f"Lisätään codespacen kokeilu")
+
